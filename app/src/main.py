00339995431802b920636e77967e2e232ecc9770
gost1k <gost1k337@gmail.com>
2020-11-19 10:29:43 +0300

    add migrations and db

--- app/src/main.py
+++ app/src/main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from . import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
+# migrate = Migrate()
 
 
 @app.route('/')
 def home():
-    return 'hello wsdaorld'
+    return 'hello world'
